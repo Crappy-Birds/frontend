bf560b32c21c72ee20c6a33155f225e5d011503e
fafifox <fafifox@protonmail.com>
2021-10-08 10:35:17 +0200

    Add open graph meta tags

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -3,16 +3,12 @@ import Image from 'next/image'
 const Hero = () => {
   return (
     <div className="relative h-full md:pb-4">
-      {/* <div className="absolute inset-x-0 bottom-0 bg-dark-400 h-1/3" /> */}
       <div className="h-full mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="relative w-full h-full shadow-xl sm:overflow-hidden sm:rounded-2xl">
           <Image
-            /* className="object-cover w-full h-full" */
             className="object-fill w-auto h-full objec md:object-cover"
-            src="/images/illus_sketch_02.png"
+            src="/images/crappy-birds-in-the-nest.png"
             alt="Crappy Birds having fun in The Nest"
-            /* width={1529}
-            height={775} */
             width={1440}
             height={720}
             quality="100"
