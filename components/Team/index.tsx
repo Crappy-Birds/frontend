794780a309e979faa096e908a1d6ee1c7d1292a4
fafifox <fafifox@protonmail.com>
2021-11-16 13:54:20 +0100

    Add Team github profile

--- components/Team/index.tsx
+++ components/Team/index.tsx
@@ -10,6 +10,7 @@ const people = [
     imageUrl: Fafifox,
     twitterUrl: 'https://twitter.com/fafif0x',
     linkedinUrl: 'https://www.linkedin.com/in/hugo-malatrait-9618b7135/',
+    githubUrl: 'https://github.com/fafifox',
   },
   {
     name: 'yam',
@@ -18,6 +19,7 @@ const people = [
     imageUrl: Yam,
     twitterUrl: 'https://twitter.com/CaptaineDoge',
     linkedinUrl: 'https://www.linkedin.com/in/saad-el-madafri-4ab208190/',
+    githubUrl: 'https://github.com/yamidevs',
   },
 ]
 
@@ -85,6 +87,29 @@ const Team = ({}) => {
                           <path
                             fillRule="evenodd"
                             d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href={person.githubUrl}
+                        className="text-gray-400 hover:text-gray-500"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <span className="sr-only">Github</span>
+                        <svg
+                          className="w-5 h-5"
+                          aria-hidden="true"
+                          fill="currentColor"
+                          viewBox="0 0 24 24"
+                        >
+                          <title>GitHub</title>
+                          <path
+                            fillRule="evenodd"
+                            d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
                             clipRule="evenodd"
                           />
                         </svg>
