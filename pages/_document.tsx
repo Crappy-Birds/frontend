b8ef91c96fd8d9867facc74fee760bf7053f3f2b
fafifox <fafifox@protonmail.com>
2021-11-28 12:26:33 +0100

    head crossorigin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -78,7 +78,7 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            crossOrigin="anonymous"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,400;0,500;0,600;0,700;1,600;1,800&display=swap"
