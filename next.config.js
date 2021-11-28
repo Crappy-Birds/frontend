546c1459faf456314c2b02f2dd3977172522c56f
fafifox <fafifox@protonmail.com>
2021-11-28 13:06:21 +0100

    Update Content-Security-Policy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: "default-src 'self' https://vitals.vercel-insights.coms", // Your CSP Policy
+  },
+]
+
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
