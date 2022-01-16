1cf44b86d7d243fa0dfc2e31ad2069b064abcd43
fafifox <fafifox@protonmail.com>
2022-01-16 10:31:06 +0100

    add Plausible analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app'
+import PlausibleProvider from 'next-plausible'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="crappybirds.io">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
 export default MyApp
