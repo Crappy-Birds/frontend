13526dbef7e99918f53d2225b12fedf98982509d
fafifox <fafifox@protonmail.com>
2022-04-07 17:14:15 +0200

    Add Instagram link to footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaDiscord, FaTwitter, FaGithub } from 'react-icons/fa'
+import { FaDiscord, FaTwitter, FaGithub, FaInstagram } from 'react-icons/fa'
 
 const navigation = [
   {
@@ -11,6 +11,11 @@ const navigation = [
     name: 'Twitter',
     href: 'https://twitter.com/crappybirds',
     icon: FaTwitter,
+  },
+  {
+    name: 'Instagram',
+    href: 'https://www.instagram.com/crappybirds.io',
+    icon: FaInstagram,
   },
   {
     name: 'Github',
