181f73968613845646d47818bb3f7465bb6328c1
fafifox <fafifox@protonmail.com>
2022-05-12 16:31:29 +0200

    Update About game copy

--- components/AboutGame/index.tsx
+++ components/AboutGame/index.tsx
@@ -1,25 +1,5 @@
 import Image from 'next/image'
-import { FaBook, FaRegNewspaper } from 'react-icons/fa'
 import CrappyBirdsOnline from '../../public/images/crappy-birds-online-game.jpg'
-
-const socialFeatures = [
-  {
-    id: 1,
-    name: 'Documentation',
-    description: 'Learn more about the project.',
-    icon: FaBook,
-    bg: 'bg-zinc-500',
-    link: 'https://docs.crappybirds.io/',
-  },
-  {
-    id: 1,
-    name: 'Litepaper',
-    description: 'Learn more about the project.',
-    icon: FaRegNewspaper,
-    bg: 'bg-zinc-500',
-    link: '/docs/crappy-birds-litepaper.pdf',
-  },
-]
 
 const AboutGame = () => {
   return (
@@ -50,50 +30,44 @@ const AboutGame = () => {
                 Crappy Birds Online
               </h2>
               <p>
-                Crappy Birds Online is a fun-first 2D Side-Scrolling MMORPG
+                Crappy Birds is a fun and social 2D Side-Scrolling MMORPG
                 leveraging NFTs for cosmetic items, lands and other properties.
               </p>
               <ul className="ml-6 space-y-2 list-disc">
                 <li>Explore a vast interconnected world</li>
+                <li>Beat up monsters and dungeon bosses</li>
                 <li>Customize your character</li>
                 <li>Socialize and hang out with others</li>
-                <li>Fight vicious enemies and bosses</li>
-                <li>Scavenge and recycle waste for raw materials</li>
-                <li>Craft equipment, weapons and gadgets to get stronger</li>
-                <li>Run your own business in a player-driven economy</li>
-                {/* <li>2D Side-scrolling MMORPG</li>
-                <li>Free-to-Play-and-Earn</li>
-                <li>Target platform PC, controller friendly</li>
-                <li>Inspired by Maplestory, Hollow Knight, Flappy Birds</li> */}
+                <li>
+                  Scavenge for waste and recycle it into raw materials used in
+                  crafts
+                </li>
+                <li>
+                  Craft equipment, weapons and gadgets to enhance your character
+                </li>
+                <li>Trade and earn, as part of an open-world economy</li>
               </ul>
-            </div>
-            <dl className="mt-8 space-y-4">
-              {socialFeatures.map((item) => (
-                <div
-                  key={item.id}
-                  className="relative p-4 transition duration-500 ease-out bg-black bg-opacity-0 rounded-lg hover:bg-opacity-5 group hover:translate-x-4"
+              <p>
+                Learn more :{' '}
+                <a
+                  className="underline"
+                  href="https://docs.crappybirds.io/genesis-collection/roadmap"
+                  target="_blank"
+                  rel="noreferrer"
                 >
-                  <dt>
-                    <div
-                      className={`absolute flex items-center justify-center w-12 h-12 text-white rounded-md ${item.bg}`}
-                    >
-                      <item.icon className="w-6 h-6" aria-hidden="true" />
-                    </div>
-                    <a
-                      className="ml-16 text-lg font-medium leading-6 before:absolute before:top-0 before:left-0 before:w-full before:h-full before:content-['']"
-                      href={item.link}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {item.name}
-                    </a>
-                  </dt>
-                  <dd className="mt-1 ml-16 text-base text-gray-600 duration-500 group-hover:text-gray-700">
-                    {item.description}
-                  </dd>
-                </div>
-              ))}
-            </dl>
+                  Documentation
+                </a>
+                ,{' '}
+                <a
+                  className="underline"
+                  href="https://crappybirds.io/docs/crappy-birds-litepaper.pdf"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Litepaper
+                </a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
